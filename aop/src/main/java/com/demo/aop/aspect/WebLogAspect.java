c345a2f0769c59a02ee3c16a1ee6054b2b0363bd
CPPCXX <xinxin_chen@cathaypacific.com>
2018-11-19 15:06:48 +0800

    add aop demo

---
+++ aop/src/main/java/com/demo/aop/aspect/WebLogAspect.java
@@ -0,0 +1,62 @@
+package com.demo.aop.aspect;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.jboss.logging.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class WebLogAspect {
+
+    private Logger logger = Logger.getLogger(getClass());
+
+
+    ThreadLocal<Long> startTime = new ThreadLocal<>();
+
+
+    /**
+     * controller包下的所有函数
+     */
+    @Pointcut("execution(public * com.demo.aop.controller.*.*(..))")
+    public void webLog(){}
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        startTime.set(System.currentTimeMillis());
+
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        logger.info("URL : " + request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : " + request.getMethod());
+        logger.info("IP : " + request.getRemoteAddr());
+        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        logger.info("RESPONSE : " + ret);
+        logger.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+
+    }
+
+
+    //ref :http://blog.didispace.com/springbootaoplog/
+    //https://niels.nu/blog/2017/spring-boot-aop.html
+
+}
